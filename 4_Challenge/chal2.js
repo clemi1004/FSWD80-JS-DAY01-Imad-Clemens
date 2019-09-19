b32848c14e9c6dd68dd8a35852cd6d1573cda580
Imad <latchimad@gmail.com>
2019-09-19 15:09:16 +0200

    challenge 3

---
+++ 4_Challenge/chal2.js
@@ -0,0 +1,4 @@
+var str = "Properties,$a$set$of$immutable$values,$are$passed$to$a$component\'s$renderer$as$properties$in$its $HTML$tag.$A$component$cannot$directly$modify$any$properties$passed$to$it,$but$can$be$passed$ callback$functions$that$do$modify$values.$This$mechanism's$promise$is$expressed$as$\"properties $flow$down;$actions$flow$up\".";
+
+var res = str.replace( /\$/g, " ");
+document.write(res);
