3965700dd53b5f0af1f77a4aab5ba312f0a247b6
Imad <latchimad@gmail.com>
2019-09-19 11:50:48 +0200

    interm3 com
    completed

--- 2_Intermediate/js/interm2.js
+++ 2_Intermediate/js/interm2.js
@@ -1,9 +1,15 @@
 var norange = [ 'apple', 'banana', 'kiwi' ];
-
+var unsort = [ 'monkey', 'horse', 'dog' ];
 console.log( norange.slice( 0, 3 ));
-norange.push('orange')
+norange.push('orange');
 console.log( norange.slice( 0, 4 ));
 console.log( norange.slice( 0, 3 ));
 
 /* alternative use push and pop method*/
+
+console.log(unsort);
+console.log(unsort.sort());
+unsort.push('cat');
+console.log(unsort.sort());
+
 
