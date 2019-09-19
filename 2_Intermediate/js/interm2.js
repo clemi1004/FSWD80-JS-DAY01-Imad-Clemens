2978a06a64f76477f3dff01b3398044389bee482
Imad <latchimad@gmail.com>
2019-09-19 11:37:45 +0200

    interm2
    done advanced

---
+++ 2_Intermediate/js/interm2.js
@@ -0,0 +1,9 @@
+var norange = [ 'apple', 'banana', 'kiwi' ];
+
+console.log( norange.slice( 0, 3 ));
+norange.push('orange')
+console.log( norange.slice( 0, 4 ));
+console.log( norange.slice( 0, 3 ));
+
+/* alternative use push and pop method*/
+
